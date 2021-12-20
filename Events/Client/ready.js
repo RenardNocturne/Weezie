faaf6b926015f0649abf0cc26d3b4c82eeac627b
Renard Nocturne <yanicehgpro@yahoo.com>
2021-12-20 19:33:39 +0100

    feat: :sparkles: Ajout d'un statut.

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,3 +1,27 @@
 module.exports = async (client) => {
     console.log("🚀 Bot successfully logged in !");
+
+    const activitiesTypes = [
+        'WATCHING',
+        'PLAYING',
+    ];
+
+    let i = 0;
+    setInterval(() => {
+        let activities = [
+            `${client.guilds.cache.get("825760704241991752").memberCount} membres !`,
+            'lancer des boules de neige sur Muzikae...',
+        ];
+
+        const newActivity = activities[i];
+        const newActivityType = activitiesTypes[i];
+        client.user.setPresence({ activities: [{ name: newActivity, type: newActivityType }], status: 'online'})
+
+        if (i < 1) {
+            i++;
+        } else {
+            i = 0;
+        }
+
+    }, 10000);
 }
