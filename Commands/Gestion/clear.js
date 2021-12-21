c1ec2f283a98913a8a53c9b9347bc974270a347f
Renard Nocturne <yanicehgpro@yahoo.com>
2021-12-21 21:05:19 +0100

    feat: :sparkles: Ajout des subCommands à clear + Ajout de /clear channel !

--- Commands/Gestion/clear.js
+++ Commands/Gestion/clear.js
@@ -7,21 +7,49 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('clear')
 		.setDescription('Nettoie un nombre défini de messages dans le salon !')
-        .addIntegerOption(option => option.setName("messages").setDescription("Nombre de messages à supprimer").setRequired(true)),
+        .addSubcommand(messages => messages
+            .setName("messages")
+            .setDescription("Supprime un nombre défini de messages dans le salon actuel.")
+            .addIntegerOption(option => option
+                .setName("nombre")
+                .setDescription("Nombre de messages à supprimer compris entre 1 et 99.")
+                .setRequired(true))
+        )
+        .addSubcommand(channel => channel
+            .setName("channel")
+            .setDescription("Supprime tous les messages de ce salon.")
+        ),
 	async execute(client, interaction) {
-		const messagesToDelete = interaction.options.data[0].value
+        switch (interaction.options.getSubcommand()) {
+            case "channel":
+                if (!interaction.channel.isThread()) interaction.channel.clone()
+                .then(newChannel => {
+                    newChannel.setPosition(interaction.channel.position)
+                    .then(() => {
+                        interaction.channel.delete()
+                        newChannel.send({content: ':fire: ``Salon réinitialisé !``'})
+                        .then(m => setTimeout(() => {
+                            m.delete().catch(err => console.log(err))
+                        }, 10000))
+                    })
+                })
+                break;
+            case "messages":
+                const messagesToDelete = interaction.options.getInteger("nombre")
 
-        if (0 >= messagesToDelete || messagesToDelete >= 100) return interaction.reply({content: ':x: Merci de fournir une valeur comprise entre 1 et 99 !', ephemeral: true});
+                if (0 >= messagesToDelete || messagesToDelete >= 100) return interaction.reply({content: ':x: Merci de fournir une valeur comprise entre 1 et 99 !', ephemeral: true});
 
-        interaction.channel.bulkDelete(messagesToDelete, true)
-        .then(async messagesDeleted => {
-            if (messagesDeleted.size === 0) return interaction.reply({content: ':x: Impossible de supprimer des messages qui datent de plus de deux semaines !', ephemeral: true})
+                interaction.channel.bulkDelete(messagesToDelete, true)
+                .then(async messagesDeleted => {
+                    if (messagesDeleted.size === 0) return interaction.reply({content: ':x: Impossible de supprimer des messages qui datent de plus de deux semaines !', ephemeral: true})
 
-            await interaction.reply({content: `:fire: \`${messagesDeleted.size} ${messagesDeleted.size > 1 ? 'messages ont été supprimés' : 'message a été supprimé'} !\``})
-            .then(() => setTimeout(() => {
-                interaction.deleteReply().catch(err => console.log(err))
-            }, 1000))
-        })
+                    await interaction.reply({content: `:fire: \`${messagesDeleted.size} ${messagesDeleted.size > 1 ? 'messages ont été supprimés' : 'message a été supprimé'} !\``})
+                    .then(() => setTimeout(() => {
+                        interaction.deleteReply().catch(err => console.log(err))
+                    }, 1000))
+                })
+                break; 
+        }
     },
     userPerms: [Permissions.FLAGS.MANAGE_MESSAGES],
     userPermsFR: ["Gérer les messages"]
