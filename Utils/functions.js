a5380ded52afca686903af9aa19319856ffc74da
Renard Nocturne <yanicehgpro@yahoo.com>
2021-12-23 19:10:32 +0100

    feat: :zap: Ajout d'un possible embedMaker pour les erreurs.

--- Utils/functions.js
+++ Utils/functions.js
@@ -1,4 +1,14 @@
+const { MessageEmbed } = require("discord.js")
+
 module.exports = client => {
     client.defaultColor = "E7C368"
     client.errorColor = "DE2916"
+
+    // client.embedError = (title = "❌ Une erreur est survenue !", desc = "", footer = "Désolé !") => {
+    //     return new MessageEmbed()
+    //         .setTitle(title)
+    //         .setDescription(desc)
+    //         .setColor(client.errorColor)
+    //         .setFooter(footer, client.user.displayAvatarURL());
+    // }
 }
