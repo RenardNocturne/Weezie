d28701396c900d27734f4155531b89554dea3600
RenardNocturne <RenardNocturne@outlook.com>
2022-04-30 17:57:21 +0200

    fix: :bug: Correction d'un petit bug sur le footer.

--- Events/Interactions/Buttons/Polls/vote.js
+++ Events/Interactions/Buttons/Polls/vote.js
@@ -1,8 +1,14 @@
 const { writeFile, readFileSync } = require("fs");
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, Client, ButtonInteraction } = require("discord.js");
 
 module.exports = {
     name: "vote",
+    /**
+     * 
+     * @param {Client} client 
+     * @param {ButtonInteraction} interaction 
+     * @returns 
+     */
     async execute (client, interaction) {
         const polls = JSON.parse(readFileSync('./Utils/Data/polls.json'));
 
@@ -10,11 +16,8 @@ module.exports = {
         const pollID = args[0];
         const vote = args[1];
         
-        console.log(polls);
         const pollsMap = client.JSONToMap(polls);
         const poll = pollsMap.get(pollID);
-        console.log(args);
-        console.log(pollsMap, poll, pollID);
         if (poll.hasVoted.includes(interaction.user.id)) return interaction.reply({content: "❌ Vous avez déjà voté !", ephemeral: true});
 
         poll.hasVoted.push(interaction.user.id);
@@ -26,12 +29,11 @@ module.exports = {
             console.log("The file was saved!");
         });
 
-
         const newEmbed = new MessageEmbed()
             .setAuthor("Sondage !", interaction.guild.iconURL())
             .setDescription(`**❓ Question:** ${poll.question} \n \n 1️⃣ **Option 1:** ${poll.option1} \`${Math.round(poll.opt1/poll.total*100)}%\` \n \n 🏳 **Neutre à ${Math.round(poll.opt0/poll.total*100)}%** \n \n 2️⃣ **Option 2:** ${poll.option2} \`${Math.round(poll.opt2/poll.total*100)}%\` \n \n *${poll.total} participants !*`)
             .setColor(client.defaultColor)
-            .setFooter(`Sondage de ${interaction.user.username}`, interaction.user.displayAvatarURL())
+            .setFooter(interaction.message.embeds[0].footer.text, interaction.message.embeds[0].footer.iconURL())
 
         interaction.message.edit({embeds: [newEmbed]})
     
