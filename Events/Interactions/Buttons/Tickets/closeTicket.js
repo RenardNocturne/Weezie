47ab9fa50de48c29a806b468f95fe13e9cb8bf22
RenardNocturne <RenardNocturne@outlook.com>
2022-05-16 19:59:32 +0200

    fix: :fire: Tout le monde peut fermer un ticket.

--- Events/Interactions/Buttons/Tickets/closeTicket.js
+++ Events/Interactions/Buttons/Tickets/closeTicket.js
@@ -3,7 +3,6 @@ const { MessageEmbed, MessageActionRow, MessageButton, MessageAttachment } = req
 module.exports =  {
     name: "closeTicket",
     async execute (client, interaction) {
-        if (!interaction.member.roles.cache.has(client.config.IDs.roles.admins) || !interaction.member.roles.cache.has(client.config.IDs.roles.mods)) return interaction.reply({content: "❌ Vous n'avez pas le rôle requis !", ephemeral: true})
         interaction.channel.delete().catch(err => client.error(err))
     }
 }
