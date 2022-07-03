3d3cd43c9d496dd6704cc4fa710f3c03216fd189
RenardNocturne <RenardNocturne@outlook.com>
2022-07-03 20:58:40 +0200

    :recycle: Ajout d'une protection avant de mentionner.

--- Events/Messages/partnership.js
+++ Events/Messages/partnership.js
@@ -6,6 +6,7 @@ const { Client, Message } = require("discord.js")
  * @param {Message} message 
  */
 module.exports = (client, message) => {
+    if (!message.member.roles.has(client.config.IDs.roles.partnershipTicket)) return
     message.channel.send(`Notification pour <@&${client.config.IDs.roles.partenairesNotifs}> !`)
     message.member.roles.remove(client.config.IDs.roles.partnershipTicket)
 }
